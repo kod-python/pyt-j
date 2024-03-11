d072cc08222d2687055a00542f0bbce0bc5de0ce
kod-python <kelalice931@gmail.com>
2024-03-11 13:59:54 +0000

    add today

---
+++ sign-login-page.py
@@ -0,0 +1,29 @@
+print("create your account")
+
+username = "admin"
+password = "admin"
+usernameInput = input("Input username: ")
+passwordINput = input("Input password: ")
+
+
+if (usernameInput == username and passwordINput == password ):
+    print("user admin created succesfully")
+    
+print("login")
+
+username = username
+password = password
+
+
+usernameInput1 = input("Input username: ")
+passwordINput1 = input("Input password: ")
+
+if (usernameInput1 == username and passwordINput1 == password):
+    print("user logged in successful")
+ 
+else:
+    print("invalid credentials")    
+
+ 
+    
+    
